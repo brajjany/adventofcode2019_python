6d0254eb122b45a3a536ba48b25d2a285aa4ef53
Brian <brian.ye22@gmail.com>
2020-04-15 21:22:45 +0200

    solve day1b

--- src/day1.py
+++ src/day1.py
@@ -1,43 +1,43 @@
-'''
---- Day 1: The Tyranny of the Rocket Equation ---
-Santa has become stranded at the edge of the Solar System while delivering presents to other planets!
-
-To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs you to bring him measurements from fifty stars.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-The Elves quickly load you into a spacecraft and prepare to launch.
-
-At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
-
-Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
-
-For example:
-
-For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-For a mass of 1969, the fuel required is 654.
-For a mass of 100756, the fuel required is 33583.
-The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
-
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
-'''
 import os
 import math
 
 
-def day1_a():
+def get_data():
     current_dir = os.getcwd()
     df = 'day1_data'
-    fuel = 0
 
     # get data
     f = open(current_dir + '/data/{}'.format(df))
     masses = f.readlines()
+    return masses
 
+
+def day_one_a():
+    masses = get_data()
+    fuel = 0
     for i in range(len(masses)):
         masses[i] = masses[i].replace("\n", "")
         masses[i] = int(masses[i])
         fuel = fuel + (math.floor(masses[i] / 3) - 2)
 
     return fuel
+
+
+def day_one_b():
+    masses = get_data()
+    fuel = 0
+    for i in range(len(masses)):
+        masses[i] = masses[i].replace("\n", "")
+        masses[i] = int(masses[i])
+
+        # divide new fuel if >= 0
+
+        fuel = fuel + day_one_b_recursive_sum(math.floor(masses[i] / 3) - 2)
+    return fuel
+
+
+def day_one_b_recursive_sum(n):
+    if n < 0:
+        return 0
+    else:
+        return n + day_one_b_recursive_sum(math.floor(n/3)-2)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import day1
 
 
 def main():
-    print("Day 1 part 1 answer: {}".format(day1.day1_a()))
+    print("Day 1 part 1 answer: {}".format(day1.day_one_a()))
+    print("Day 1 part 2 answer: {}".format(day1.day_one_b()))
 
 
 if __name__ == '__main__':
