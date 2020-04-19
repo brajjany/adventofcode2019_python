f2733ca37c56e7bdbc909191d1334272a632f484
Brian <brian.ye22@gmail.com>
2020-04-19 23:44:58 +0200

    shit

--- src/day3.py
+++ src/day3.py
@@ -5,9 +5,9 @@ def get_coordinates(init, data):
     """return dict of lines with coordinates, i.e. {line1 : [(x_1,y_1), (x_2,y_2)], line2 : [(x_2,y_2),(x_3,y_3]}"""
     lines = {}
     first_coordinates = init
-    second_coordinates = init
 
     for i, element in enumerate(data):
+        coord_values = []
         if element[0] == 'R':
             x = first_coordinates[0] + int(element[1:])
             y = first_coordinates[1]
@@ -32,12 +32,25 @@ def get_coordinates(init, data):
 
         if "Line" + str(i) not in lines.keys():
             lines["Line" + str(i)] = (first_coordinates, second_coordinates)
+
+            if element[0] == 'R' or element[0] == 'L':
+                for j in range(int(element[1:])):
+                    lines["Line" + str(i)] = coord_values.append((first_coordinates+j, second_coordinates))
+                    # TODO create dict of all positions for each line
+
         else:
             raise Exception("Something went wrong.")
 
         first_coordinates = second_coordinates
 
     return lines
+
+
+def check_line_direction(p, q):
+    if p[0] == q[0]:
+        return "v"
+    else:
+        return "h"
 
 
 def get_line_intersection(line1, line2):
@@ -53,8 +66,8 @@ def get_line_intersection(line1, line2):
         """Gets determinant of 2x2 matrix. If determinant = 0 then no or inf no. of intersections"""
         return a[0] * b[1] - a[1] * b[0]
 
-    x_diff = (line1[0][0]-line1[1][0], line2[0][0]-line2[1][0])
-    y_diff = (line1[0][1]-line1[1][1], line2[0][1]-line2[1][1])
+    x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
     div = get_deter(x_diff, y_diff)
 
@@ -62,14 +75,13 @@ def get_line_intersection(line1, line2):
         return -1, (-1, -1)
 
     elif div != 0:
-        x = ((get_deter(line1[0], line1[1]) * x_diff[1]) -
-             (x_diff[0] * get_deter(line2[0], line2[1]))) / div
-        y = ((get_deter(line1[0], line1[1]) * y_diff[1]) -
-             (y_diff[0] * get_deter(line2[0], line2[1]))) / div
+        x = int(((get_deter(line1[0], line1[1]) * x_diff[1]) -
+                 (x_diff[0] * get_deter(line2[0], line2[1]))) / div)
+        y = int(((get_deter(line1[0], line1[1]) * y_diff[1]) -
+                 (y_diff[0] * get_deter(line2[0], line2[1]))) / div)
+        distance = abs(x - 0) + abs(y - 0)
+        intersection = (x, y)
 
-        distance = abs(x-0) + abs(y-0)
-
-        intersection = 0, (x, y)
         return distance, intersection
 
     else:
@@ -79,14 +91,13 @@ def get_line_intersection(line1, line2):
 def day_three_a():
     data = get_data_comma(data='day3_data')
     output = []
+    lines = []
+    distances = []
     init = (0, 0)
 
     # Create array of coordinates
     coordinates_a = get_coordinates(init=init, data=data[0])
     coordinates_b = get_coordinates(init=init, data=data[1])
-
-    # Get intersections
-    # intersect = get_line_intersection(coordinates_a["Line0"], coordinates_b["Line0"])
 
     for p in coordinates_a:
         for q in coordinates_b:
@@ -96,17 +107,22 @@ def day_three_a():
                 continue
             else:
                 output.append(inter)
+                lines.append((p, q))
+                distances.append(d)
 
-    return 0, coordinates_a, coordinates_b, output
+    return 0, coordinates_a, coordinates_b, output, lines, distances
 
 
-'''
-print('R sum {} len {}: {}'.format(sum(day_three_a()[0]), len(day_three_a()[0]), day_three_a()[0]))
-print('L sum {} len {}: {}'.format(sum(day_three_a()[1]), len(day_three_a()[1]), day_three_a()[1]))
-print('U sum {} len {}: {}'.format(sum(day_three_a()[2]), len(day_three_a()[2]), day_three_a()[2]))
-print('D sum {} len {}: {}'.format(sum(day_three_a()[3]), len(day_three_a()[3]), day_three_a()[3]))
-print('Values {} | {}'.format(len(day_three_a()[4]),day_three_a()[4]))
-print('Directions {} | {}'.format(len(day_three_a()[5]),day_three_a()[5]))
+print(day_three_a()[1])
+print(day_three_a()[2])
+print("-----")
+print(day_three_a()[3][1])
+print(day_three_a()[4][1])
+print(day_three_a()[5][1])
 
-print('ALL {}'.format(len(get_data_comma(data='day3_data')[0])))'''
-print(day_three_a()[3])
+'''import matplotlib.pyplot as plt
+
+plt.scatter([995, 995, 0, -999], [0, -993, 0, 0])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()'''
