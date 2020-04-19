e4b86c065b36b3524bd1c0ded353f993274dc738
Brian <brian.ye22@gmail.com>
2020-04-19 15:34:50 +0200

    Complete day2b

--- src/day2.py
+++ src/day2.py
@@ -23,18 +23,52 @@ def get_opcode(i, x, y):
         return -9999999
 
 
-def day_two():
+def day_two_a(noun=0, verb=0):
     # init local vars
     input_data = get_data().tolist()
-    input_data[1] = 12
-    input_data[2] = 2
+    input_data[1] = noun  # noun
+    input_data[2] = verb  # verb
     i = 0
 
     while i < len(input_data):
         if input_data[i] == 99:
             break
         else:
-            input_data[input_data[i+3]] = get_opcode(input_data[i], input_data[input_data[i+1]], input_data[input_data[i+2]])
+            input_data[input_data[i + 3]] = get_opcode(input_data[i], input_data[input_data[i + 1]],
+                                                       input_data[input_data[i + 2]])
         i = i + 4
 
-    return input_data
+    # output, noun, verb
+    return input_data[0], input_data[1], input_data[2]
+
+
+"""
+memory = list of integers
+address = position in memory
+Opcode (1,2,99) is beginning of an instruction
+Parameters = values used immediately after an opcode
+Current instruction = instruction pointer (init=0), increases by number of values in instruction (always 4, 1 opcode + 3 parameters)
+"""
+
+
+def day_two_b():
+    noun_index = 0
+    verb_index = 0
+    output = -1
+
+    while noun_index < 99:
+
+        while verb_index < 99:
+            output, noun, verb = day_two_a(noun_index, verb_index)
+            if output == 19690720:
+                break
+            else:
+                verb_index += 1
+
+        if output == 19690720:
+            break
+        else:
+            verb_index = 0
+            noun_index += 1
+
+    return output, noun_index, verb_index, 100*noun_index + verb_index
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-import day1
+import day1, day2
 
 
 def main():
+    print("Calculating day1...")
     print("Day 1 part 1 answer: {}".format(day1.day_one_a()))
     print("Day 1 part 2 answer: {}".format(day1.day_one_b()))
+    print("Calculating day2...")
+    print("Day 2 part 1 answer: {}".format(day2.day_two_a(12, 2)))
+    print("Day 2 part 2 answer: {}".format(day2.day_two_b()))
+    print("Calculating...")
+    print("Done!")
 
 
 if __name__ == '__main__':
